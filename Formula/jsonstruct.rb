e6e4c265885d76c76c671322e3b0f6b3207a9c75
田奕 <tianyi.tylor@bytedance.com>
2025-07-09 16:09:27 +0800

    feat: add jsonstruct.rb

--- Formula/jsonstruct.rb
+++ Formula/jsonstruct.rb
@@ -3,15 +3,15 @@ class Jsonstruct < Formula
 
   desc "CLI tool to pretty-print JSON and decode JWTs"
   homepage "https://github.com/TylorTian/jsonstruct"
-  url "https://files.pythonhosted.org/packages/source/j/jsonstruct-cli/jsonstruct_cli-0.1.5.tar.gz"
-  sha256 "9a117f8b4f3c9dba7aa87a5c0113358b8b540c7ddabf2edcbbcccf6c572c7ef4"
+  url "https://files.pythonhosted.org/packages/source/j/jsonstruct-cli/jsonstruct_cli-0.1.11.tar.gz"
+  sha256 "c550397bef77e0cd6f51a969228acd91084ec6dc2be54004c9a3cfe46e4a4258"
   license "MIT"
 
   depends_on "python@3.11"
 
   resource "pyjwt" do
     url "https://files.pythonhosted.org/packages/source/p/pyjwt/pyjwt-2.10.1.tar.gz"
-    sha256 "3cc5772eb20009233caf06e9d8a0577824723b44e6648ee0a2aedb6cf9381953"
+    sha256 "c550397bef77e0cd6f51a969228acd91084ec6dc2be54004c9a3cfe46e4a4258"
   end
 
 
